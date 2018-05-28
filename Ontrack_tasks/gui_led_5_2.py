bb56a630679b119bed717a8b05c36fa91bec680e
Suriya Sandall <suriyasandall@gmail.com>
2018-05-28 21:35:58 +1000

    Add stuff

--- Ontrack_tasks/gui_led_5_2.py
+++ Ontrack_tasks/gui_led_5_2.py
@@ -13,13 +13,13 @@ blue = LED(21)
 
 ### GUI DEFINITIONS ##
 
-win = Tk()
+win = tk()
 win.title("LED Toggler")
 myFont = tkinter.font.Font(family = 'Helvetica', size = 12, weight = "bold")
 
 ### EVENT FUNCTIONS ###
 
-def ledToggle():
+def redToggle():
     if red.is_lit:
         red.off()
         redButton["text"] = "Turn LED on"
@@ -27,6 +27,28 @@ def ledToggle():
         red.on()
         redButton["text"] = "Turn LED off"
 
+def blueToggle():
+    if green.is_lit:
+        green.off()
+        greenButton["text"] = "Turn LED on"
+    else:
+        green.on()
+        greenButton["text"] = "Turn LED off"
+
+def greenToggle():
+    if blue.is_lit:
+        blue.off()
+        blueButton["text"] = "Turn LED on"
+    else:
+        blue.on()
+        blueButton["text"] = "Turn LED off"
+
 ### WIDGETS ###
-redButton = Button(win, text = "Turn LED on", font = myFont, command = ledToggle, bg = 'red', height = 1, width = 24)
+redButton = Button(win, text = "Turn LED on", font = myFont, command = redToggle, bg = 'red', height = 1, width = 24)
 redButton.grid(row=0, column = 1)
+
+greenButton = Button(win, text = "Turn LED on", font = myFont, command = greenToggle, bg = 'red', height = 1, width = 24)
+redButton.grid(row=0, column = 2)
+
+blueButton = Button(win, text = "Turn LED on", font = myFont, command = blueToggle, bg = 'red', height = 1, width = 24)
+redButton.grid(row=0, column = 3)
