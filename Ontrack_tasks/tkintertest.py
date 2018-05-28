04ccdd31fbc7f1651f4aa6dda64bc268420ad6ee
Suriya Sandall <suriyasandall@gmail.com>
2018-05-28 22:06:26 +1000

    Memes

--- Ontrack_tasks/tkintertest.py
+++ Ontrack_tasks/tkintertest.py
@@ -1,10 +1,27 @@
 import Tkinter as tk
+import tk.font
+from gpiozero import LED
+import RPi.GPIO
+RPi.GPIO.setmode(RPi.GPIO.BCM)
 
 class ExampleView(tk.Frame):
+    red = LED(20)
     def __init__(self, root):
         tk.Frame.__init__(self, root)
+        '''
         l = tk.Label(self, text="your widgets go here...", anchor="c")
         l.pack(side="top", fill="both", expand=True)
+        '''
+        redButton = Button(master, text="Turn LED On", command=redToggle)
+        redButton.pack()
+
+    def redToggle():
+        if red.is_lit:
+            red.off()
+            redButton["text"] = "Turn LED on"
+        else:
+            red.on()
+            redButton["text"] = "Turn LED off"
 
 if __name__=='__main__':
     root = tk.Tk()
