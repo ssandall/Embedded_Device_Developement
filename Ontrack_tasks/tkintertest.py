b80974272ffc785f89e027f397ea5da7f7cd922c
Suriya Sandall <suriyasandall@gmail.com>
2018-05-28 22:29:13 +1000

    meme

--- Ontrack_tasks/tkintertest.py
+++ Ontrack_tasks/tkintertest.py
@@ -6,6 +6,8 @@ RPi.GPIO.setmode(RPi.GPIO.BCM)
 class ExampleView(Frame):
     def __init__(self, root):
         red = LED(20)
+        green = LED(16)
+        blue = LED(21)
         def redToggle():
             if red.is_lit:
                 red.off()
@@ -13,11 +15,29 @@ class ExampleView(Frame):
             else:
                 red.on()
                 redButton["text"] = "Turn LED off"
+        def blueToggle():
+            if green.is_lit:
+                green.off()
+                greenButton["text"] = "Turn LED on"
+            else:
+                green.on()
+                greenButton["text"] = "Turn LED off"
+        def greenToggle():
+            if blue.is_lit:
+                blue.off()
+                blueButton["text"] = "Turn LED on"
+            else:
+                blue.on()
+                blueButton["text"] = "Turn LED off"
 
         Frame.__init__(self, root)
         redButton = Button(win, text="Turn LED On", command= redToggle)
         redButton.pack()
-
+        blueButton = Button(win, text="Turn LED On", command= blueToggle)
+        blueButton.pack()
+        greenButton = Button(win, text="Turn LED On", command= greenToggle)
+        greenButton.pack()
+        
 if __name__=='__main__':
     win = Tk()
     view = ExampleView(win)
