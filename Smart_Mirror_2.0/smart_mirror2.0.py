bdc9c59553742b88f772e55f23f1026a8eafdb58
Suriya Sandall <suriyasandall@gmail.com>
2018-05-29 23:05:10 +1000

    Added clock module sm2

--- Smart_Mirror_2.0/smart_mirror2.0.py
+++ Smart_Mirror_2.0/smart_mirror2.0.py
@@ -1,6 +1,23 @@
-from Tkinter import *
+from tkinter import *
 import time
-import requests
-import json
-import urllib2
-import praw
+
+class Clock(Frame):
+    def __init__(self, root):
+        time1 = ''
+        clock = Label(root, font=('times', 20, 'bold'), bg='green')
+        clock.pack(fill="both", expand=True)
+        self.tick()
+
+    def tick(self):
+        global time1
+        time2 = time.strftime('%H:%M:%S')
+        if time2 != time1:
+        time1 = time2
+        clock.config(text=time2)
+        clock.after(200, tick)
+
+if __name__=='__main__':
+    win = Tk()
+    view = Clock(win)
+    view.pack(side="top", fill="both", expand=True)
+    win.mainloop()
