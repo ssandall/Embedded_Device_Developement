f5cb26855ac1d6b329da47ed04b57ae3aba3d810
Suriya Sandall <suriyasandall@gmail.com>
2018-05-29 23:34:48 +1000

    global var fix

--- Smart_Mirror_2.0/smart_mirror2.0.py
+++ Smart_Mirror_2.0/smart_mirror2.0.py
@@ -4,19 +4,20 @@ import time
 xlarge_text_size = 12
 
 class Clock(Frame):
-    def __init__(self, root):
-        #clock = Label(root, font=('times', 20, 'bold'), bg='green')
-        clock= Label(self, font=('Helvetica', xlarge_text_size), fg="white", bg="black")
-        clock.pack(fill="both", expand=True)
+    def __init__(self, parent, *args, **kwargs):
+        Frame.__init__(self, parent, bg='black')
+        #time label
+        self.time1 = ''
+        self.timeLbl = Label(self, font=('Helvetica', xlarge_text_size), fg="white", bg="black")
+        self.timeLbl.pack(side=TOP, anchor=E)
         self.tick()
 
     def tick(self):
-        time1 = ''
         time2 = time.strftime('%H:%M:%S')
-        if time2 != time1:
+        if time2 != self.time1:
             time1 = time2
-        self.clock.config(text=time2)
-        clock.after(200, tick)
+            self.timeLbl.config(text=time2)
+            self.timeLbl.after(200, self.tick)
 
 if __name__=='__main__':
     win = Tk()
