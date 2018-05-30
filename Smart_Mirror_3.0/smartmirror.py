58db645f499cf60d3497ab7658893b5b358e623b
Suriya Sandall <suriyasandall@gmail.com>
2018-05-31 02:43:14 +1000

    Adding reddit

--- Smart_Mirror_3.0/smartmirror.py
+++ Smart_Mirror_3.0/smartmirror.py
@@ -195,7 +195,7 @@ class Reddit(Frame):
                      user_agent='redditapi', username='Web_Hoon')
 
             subreddit = reddit.subreddit(SUBREDDIT_SELECTION)
-            top_subreddit = subreddit.top(1)
+            top_subreddit = subreddit.hot(1)
 
             for submission in top_subreddit:
                 if not submission.stickied:
