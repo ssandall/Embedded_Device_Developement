05ed49c98260084dd834eeca86e823cb62975d8c
Suriya Sandall <suriyasandall@gmail.com>
2018-05-31 03:19:17 +1000

    Change font to white

--- Smart_Mirror_3.0/smartmirror.py
+++ Smart_Mirror_3.0/smartmirror.py
@@ -14,7 +14,7 @@ from contextlib import contextmanager
 
 #Font Variables
 font_type = 'Helvetica'
-font_colour = "CadetBlue 2"
+font_colour = "White"
 xlarge_text_size = 48
 large_text_size = 30
 medium_text_size = 20
