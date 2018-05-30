df82c9bc014902c2f552689b6d509fb3a6a83b4e
Suriya Sandall <suriyasandall@gmail.com>
2018-05-31 01:57:56 +1000

    Adding News Feature

--- Smart_Mirror_2.0/smart_mirror2.0.py
+++ Smart_Mirror_2.0/smart_mirror2.0.py
@@ -149,6 +149,52 @@ class Reddit(Frame):
             traceback.print_exc()
             print ("Error: %s. This is a BIG REDDIT ERROR.") % f
 '''
+class News(Frame):
+    def __init__(self, parent, *args, **kwargs):
+        Frame.__init__(self, parent, *args, **kwargs)
+        self.config(bg='black')
+        self.title = 'News'
+        self.newsLbl = Label(self, text=self.title, font=(font_type, medium_text_size), fg=font_colour, bg="black")
+        self.newsLbl.pack(side=TOP, anchor=W)
+        self.headlinesContainer = Frame(self, bg="black")
+        self.headlinesContainer.pack(side=TOP)
+        self.get_headlines()
+
+    def get_headlines(self):
+        try:
+            for widget in self.headlinesContainer.winfo_children():
+                widget.destroy()
+            if news_country_code == None:
+                headlines_url = "https://news.google.com/news?ned=au&output=rss"
+            else:
+                headlines_url = "https://news.google.com/news?ned=%s&output=rss" % news_country_code
+
+            feed = feedparser.parse(headlines_url)
+
+            for post in feed.entries[0:5]:
+                headline = NewsHeadline(self.headlinesContainer, post.title)
+                headline.pack(side=TOP, anchor=W)
+        except Exception as e:
+            traceback.print_exc()
+            print "Error: %s. Cannot get news." % e
+
+        self.after(600000, self.get_headlines)
+class NewsHeadline(Frame):
+    def __init__(self, parent, event_name=""):
+        Frame.__init__(self, parent, bg='black')
+
+        image = Image.open("assets/Newspaper.png")
+        image = image.resize((25, 25), Image.ANTIALIAS)
+        image = image.convert('RGB')
+        photo = ImageTk.PhotoImage(image)
+
+        self.iconLbl = Label(self, bg='black', image=photo)
+        self.iconLbl.image = photo
+        self.iconLbl.pack(side=LEFT, anchor=N)
+
+        self.eventName = event_name
+        self.eventNameLbl = Label(self, text=self.eventName, font=(font_type, small_text_size), fg=font_colour, bg="black")
+        self.eventNameLbl.pack(side=LEFT, anchor=N)
 class FullscreenWindow:
 
     def __init__(self):
