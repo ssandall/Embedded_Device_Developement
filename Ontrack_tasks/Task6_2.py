20025405319f3df6d24538f5fbc7524d82ad7b40
Suriya Sandall <suriyasandall@gmail.com>
2018-06-03 22:51:35 +1000

    Add task 6.2

--- Ontrack_tasks/Task6_2.py
+++ Ontrack_tasks/Task6_2.py
@@ -2,11 +2,11 @@ import RPi.GPIO as GPIO
 import time
 
 try:
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(16, GPIO.OUT)
-    GPIO.output(16, GPIO.HIGH)
 
     while 1:
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(16, GPIO.OUT)
+        GPIO.output(16, GPIO.HIGH)
         GPIO.output(16, GPIO.HIGH)
         time.sleep(0.25)
 
