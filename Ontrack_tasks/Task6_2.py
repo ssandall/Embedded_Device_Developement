f1894966f16b60ed518cb0c076c861517ade44d0
Suriya Sandall <suriyasandall@gmail.com>
2018-06-03 22:45:58 +1000

    Add task 6.2

---
+++ Ontrack_tasks/Task6_2.py
@@ -0,0 +1,12 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(16, GPIO.OUT)
+GPIO.output(16, GPIO.HIGH)
+
+while 1:
+    GPIO.output(16, GPIO.HIGH)
+    time.sleep(0.25)
+except KeyboardInterrupt:
+GPIO.cleanup()
